9d4651b02ad81f538af96b99b6d4adcbfd82219e
kalemadaniel <kalemadaniel2014@gmail.com>
2021-08-23 14:47:27 +0200

    first commit

---
+++ src/demo/java/json/data/file/DemoJavaJsonDataFile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.java.json.data.file;
+
+/**
+ *
+ * @author Ulb-Kalema
+ */
+public class DemoJavaJsonDataFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
