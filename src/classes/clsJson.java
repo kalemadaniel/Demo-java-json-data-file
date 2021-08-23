d07e799c9b7e1065e6e4d2d76ca1c498c5488fa7
kalemadaniel <kalemadaniel2014@gmail.com>
2021-08-23 16:18:11 +0200

    readJsonfile

--- src/classes/clsJson.java
+++ src/classes/clsJson.java
@@ -5,16 +5,43 @@
  */
 package classes;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.text.ParseException;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+
 
 /**
  *
  * @author Ulb-Kalema
  */
 public class clsJson {
+
+    
+    
    public void readJsonfile(){
-       try {
-           
-       } catch (Exception e) {
-       }
+      JSONParser jsonParser;
+        jsonParser = new JSONParser("./.json", null, true);
+      try {
+         //Parsing the contents of the JSON file
+         JSONObject jsonObject = (JSONObject) jsonParser.parse();
+         //Retrieving the array
+         JSONArray jsonArray = (JSONArray) jsonObject.get("people");
+         for(Object object : jsonArray) {
+            JSONObject record = (JSONObject) object;
+        }  
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
    }
 }
