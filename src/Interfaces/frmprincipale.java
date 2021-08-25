7c320b7938a04fdeb0fef88d734781b0e81410c2
kalemadaniel <kalemadaniel2014@gmail.com>
2021-08-25 13:59:15 +0200

    add loadfiledatajson interfaces

--- src/Interfaces/frmprincipale.java
+++ src/Interfaces/frmprincipale.java
@@ -34,10 +34,14 @@ public class frmprincipale extends javax.swing.JFrame {
         jLabel9 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel11 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -69,6 +73,24 @@ public class frmprincipale extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(250, 250, 250));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel4.setBackground(new java.awt.Color(250, 250, 250));
+        jPanel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel4.setToolTipText("");
+        jPanel4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPanel4MouseClicked(evt);
+            }
+        });
+        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel11.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel11.setText("- - -");
+        jPanel4.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 50, 40));
+
+        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 70, 40));
+
         jPanel2.setBackground(new java.awt.Color(250, 250, 250));
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel2.setToolTipText("");
@@ -78,21 +100,32 @@ public class frmprincipale extends javax.swing.JFrame {
             }
         });
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/ic_autorenew_black_24dp.png"))); // NOI18N
         jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 40, 40));
 
         jLabel8.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
         jLabel8.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel8.setText("Parcourir");
+        jLabel8.setText("Charger");
         jPanel2.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 0, 100, 40));
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, 170, 40));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 60, 170, 40));
 
         jTextField1.setEditable(false);
         jTextField1.setBackground(new java.awt.Color(250, 250, 250));
-        jTextField1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
+        jTextField1.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         jTextField1.setForeground(new java.awt.Color(153, 153, 153));
         jTextField1.setText("jTextField1");
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 10, 500, 40));
+        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, 630, 40));
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, 720, 250));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 90, 740, 390));
 
@@ -108,8 +141,14 @@ public class frmprincipale extends javax.swing.JFrame {
     private void jPanel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MouseClicked
         // TODO add your handling code here:
         clsJson json=new clsJson();
-        json.ChooseJsonFile(jTextField1);
+        json.readJsonfile(jList1);
     }//GEN-LAST:event_jPanel2MouseClicked
+
+    private void jPanel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel4MouseClicked
+        // TODO add your handling code here:
+        clsJson json=new clsJson();
+        json.ChooseJsonFile(jTextField1);
+    }//GEN-LAST:event_jPanel4MouseClicked
 
     /**
      * @param args the command line arguments
@@ -148,13 +187,17 @@ public class frmprincipale extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JList jList1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- src/classes/clsJson.java
+++ src/classes/clsJson.java
@@ -6,58 +6,71 @@
 package classes;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import static java.util.Locale.filter;
 import javax.swing.JFileChooser;
+import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import jdk.nashorn.internal.parser.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
-
-
 /**
  *
  * @author Ulb-Kalema
  */
 public class clsJson {
+
     FileNameExtensionFilter filter = null;
 
-   
-//    public void ChooseJsonFile(JTextField txtPath) {
-//        JFileChooser fileChooser = new JFileChooser();
-//        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-//        filter = new FileNameExtensionFilter("*.JSON", "json", "Json");
-//        fileChooser.addChoosableFileFilter(filter);
-//        int result = fileChooser.showSaveDialog(null);
-//        if (result == JFileChooser.APPROVE_OPTION) {
-//            File selectedFile = fileChooser.getSelectedFile();
-//            String path = selectedFile.getAbsolutePath();
-//            txtPath.setText(path);
-//        }
-//    }
-    
-   public void readJsonfile(){
-     
-       
-//      JSONParser jsonParser;
-//        jsonParser = new JSONParser("./.json", null, true);
-//      try {
-//         //Parsing the contents of the JSON file
-//         JSONObject jsonObject = (JSONObject) jsonParser.parse();
-//         //Retrieving the array
-//         JSONArray jsonArray = (JSONArray) jsonObject.get("people");
-//         List<String> list = new ArrayList<String>();
-//         for(Object object : jsonArray) {
-//            JSONObject record = (JSONObject) object;
-////            list.add(record.getJSONArray(jsonArray));
-//        }  
-//      } catch (Exception e) {
-//         // TODO Auto-generated catch block
-//         e.printStackTrace();
-//      }
-   }
+    public void ChooseJsonFile(JTextField txtPath) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        filter = new FileNameExtensionFilter("*.JSON", "json", "Json");
+        fileChooser.addChoosableFileFilter(filter);
+        int result = fileChooser.showSaveDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            String path = selectedFile.getAbsolutePath();
+            txtPath.setText(path);
+        }
+    }
+
+
+
+    public void readJsonfile1() {
+       //Creating a JSONParser object
+      JSONParser jsonParser = new JSONParser();
+      try {
+         //Parsing the contents of the JSON file
+         JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("E:/players_data.json"));
+         //Retrieving the array
+         JSONArray jsonArray = (JSONArray) jsonObject.get("players_data");
+         for(Object object : jsonArray) {
+            JSONObject record = (JSONObject) object;
+            int id = Integer.parseInt((String) record.get("ID"));
+            String first_name = (String) record.get("First_Name");
+            String last_name = (String) record.get("Last_Name");
+            String date = (String) record.get("Date_Of_Birth");
+            long date_of_birth = Date.valueOf(date).getTime();
+            String place_of_birth = (String) record.get("Place_Of_Birth");
+            String country = (String) record.get("Country");
+         }  
+         System.out.println("Records inserted.....");
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+    }
 }
