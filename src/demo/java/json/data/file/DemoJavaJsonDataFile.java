f334c98abdd72c86727181beb52fec136d7657c7
kalemadaniel <kalemadaniel2014@gmail.com>
2021-08-26 09:59:46 +0200

    principal select first

--- src/demo/java/json/data/file/DemoJavaJsonDataFile.java
+++ src/demo/java/json/data/file/DemoJavaJsonDataFile.java
@@ -5,6 +5,8 @@
  */
 package demo.java.json.data.file;
 
+import Interfaces.frmprincipale;
+
 /**
  *
  * @author Ulb-Kalema
@@ -16,6 +18,25 @@ public class DemoJavaJsonDataFile {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(frmprincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(frmprincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frmprincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmprincipale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        frmprincipale pr=new frmprincipale();
+        pr.setVisible(true);
     }
     
 }
