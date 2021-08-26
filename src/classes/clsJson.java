24a1aac902b482189f04e36714d7b7a755d247ea
kalemadaniel <kalemadaniel2014@gmail.com>
2021-08-26 16:06:27 +0200

    change readJsonfile in insertjson

--- src/classes/clsJson.java
+++ src/classes/clsJson.java
@@ -6,13 +6,21 @@
 package classes;
 
 import java.io.File;
-import java.io.FileReader;
-import java.sql.Date;
+import java.sql.Connection;
 import javax.swing.JFileChooser;
-import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import jdk.nashorn.internal.parser.JSONParser;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.text.ParseException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 
 /**
  *
@@ -35,11 +43,61 @@ public class clsJson {
         }
     }
 
-
-
-    public void readJsonfile(JList lst)  {
-      
+    
+    
+    public static Connection ConnectToDB() throws Exception {
+      //Registering the Driver
+      DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+      //Getting the connection
+      String mysqlUrl = "jdbc:mysql://localhost/bd_json_msyql";
+      java.sql.Connection con = DriverManager.getConnection(mysqlUrl, "root", "MQ4k4z22MhB6vD8GvsrY87du75KiNW");
+      System.out.println("Connection established......");
+      return (Connection) con;
+   }
+    public void insertJsonfile()  {
+        // TODO code application logic here
+        //Creating a JSONParser object
+      JSONParser jsonParser = new JSONParser();
+      try {
+         //Parsing the contents of the JSON file
+         JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("./players_data.json"));
+         //Retrieving the array
+         JSONArray jsonArray = (JSONArray) jsonObject.get("players_data");
+         Connection con = ConnectToDB();
+         //Insert a row into the MyPlayers table
+         PreparedStatement pstmt = con.prepareStatement("INSERT INTO tb_population values (?, ?, ?, ?, ?, ?, ? )");
+         for(Object object : jsonArray) {
+            JSONObject record = (JSONObject) object;
+            
+            int id = Integer.parseInt((String) record.get("ID"));
+            String first_name = (String) record.get("First_Name");
+            String last_name = (String) record.get("Last_Name");
+            String date = (String) record.get("Date_Of_Birth");
+            long date_of_birth = Date.valueOf(date).getTime();
+            String place_of_birth = (String) record.get("Place_Of_Birth");
+            String country = (String) record.get("Country");
+            pstmt.setInt(1, id);
+            pstmt.setString(2, first_name);
+            pstmt.setString(3, last_name);
+            pstmt.setDate(4, new Date(date_of_birth));
+            pstmt.setString(5, place_of_birth);
+            pstmt.setString(6, country);
+            pstmt.executeUpdate();
+         }  
+         System.out.println("Records inserted.....");
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (ParseException e) {
+         e.printStackTrace();
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
     }
+
+
     
     //change readJsonfile in insertjson file
 }
---
+++ src/classes/clspersonne.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Ulb-Kalema
+ */
+public class clspersonne {
+    private int id;
+    private int age;
+    private String nom;
+    private String postnom;
+    private String prenom;
+    private String genre;
+    private String ville;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * @return the postnom
+     */
+    public String getPostnom() {
+        return postnom;
+    }
+
+    /**
+     * @param postnom the postnom to set
+     */
+    public void setPostnom(String postnom) {
+        this.postnom = postnom;
+    }
+
+    /**
+     * @return the prenom
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * @param prenom the prenom to set
+     */
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    /**
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return the ville
+     */
+    public String getVille() {
+        return ville;
+    }
+
+    /**
+     * @param ville the ville to set
+     */
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+}
